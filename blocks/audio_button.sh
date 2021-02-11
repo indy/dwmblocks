4aeb6637ae8914f38763bbf1c2d2481a53d7c74b
Inderjit Gill <email@indy.io>
2021-02-11 15:06:04 +0000

    using single dwm-audio

--- blocks/audio_button.sh
+++ blocks/audio_button.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 case $1 in
-    1) dwm-audio-dec ;;
-    2) dwm-audio-toggle ;;
-    3) dwm-audio-inc ;;
+    1) dwm-audio dec ;;
+    2) dwm-audio toggle ;;
+    3) dwm-audio inc ;;
 esac
